3104426198472c5db5f563c37dc07c459825f357
SevenDev <cristian.lee1111@gmail.com>
2021-11-26 00:07:12 +0900

    minor update

--- README.md
+++ README.md
@@ -10,16 +10,16 @@ $ npm install --save toflexible
 var toFlexible = require('toflexible');
 
 toFlexible(0.0000234234234, 3)
-// => 0.00002342
+// => 0.0000234
 
 toFlexible(0.00001201442224)
 // => 0.000012
 
 toFlexible(12330.0000234234234, 3)
-// => 12,330.00
+// => 12,330.000
 
 toFlexible(25252021, 3)
-// => 25,252,021.00
+// => 25,252,021.000
 ```
 
 Description: it will return decimals with round and fixed number of fractions, as localized format.
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const toFixed = (x, digits) => Math.round( x * (10 ** digits) ) / (10 ** digits)
+const toFixed = (x, digits) => Math.round(x * (10 ** digits)) / (10 ** digits)
 const toFlexible = (x, n = 2) => {
-	for (let i = 0; i< 15; i++) {
-  	if (toFixed(x, i) > 0) {
-    	return toFixed(x, i+n).toLocaleString(undefined, {maximumFractionDigits: 15, minimumFractionDigits: 2})
-    }
-  }
+	for (let i = 0; i < 18; i++) {
+		if(x * (10 ** i) > 0.1) {
+		  return toFixed(x, i + n).toLocaleString(undefined, { minimumFractionDigits: n, maximumFractionDigits: 18 });
+		}
+	}
 }
 
 module.exports = toFlexible
