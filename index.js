3ee2a2260577ff60ac0f099a01c6fd398d750a39
SevenDev <cristian.lee1111@gmail.com>
2021-11-26 22:26:06 +0900

    Set default locale

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const toFixed = (x, digits) => Math.round(x * (10 ** digits)) / (10 ** digits)
 const toFlexible = (x, n = 2) => {
 	for (let i = 0; i < 18; i++) {
 		if(x * (10 ** i) > 0.1) {
-		  return toFixed(x, i + n).toLocaleString(undefined, { minimumFractionDigits: n, maximumFractionDigits: 18 });
+		  return toFixed(x, i + n).toLocaleString('en-US', { minimumFractionDigits: n, maximumFractionDigits: 18 });
 		}
 	}
 }
